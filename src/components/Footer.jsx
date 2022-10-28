cd6e8e7dfc2d806ec94800b3207e31d79acca20d
Madina <madinatussupova0820@gmail.com>
2022-10-28 11:44:38 +0600

    add react components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright &copy; {year}</p>
+    </footer>
+  )
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header () {
+ return <h1 className="header-title">Keeper</h1>
+}
+
+export default Header;	
